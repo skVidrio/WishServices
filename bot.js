aea1ee6150ade7831b7a17a54e4ce4b6c347364f
ryda <60327618+skVidrio@users.noreply.github.com>
2020-02-29 16:47:26 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,4 @@ client.on('message', message => {
        }
 });
  
-client.login(process.env.NjgyNDMyMjcyOTU5Mjc1MTIz.Xlnd1g.iwKpTB-N6ur2m8uZGh_R4WJ1Q8g);//BOT_TOKEN is the Client Secret
+client.login(process.env.vHg_hPmYwBKu5O1AnpK3aoM5dndFTe-k);//BOT_TOKEN is the Client Secret
