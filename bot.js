9f3baf54a95df7ec4005006596e89dd572e2ffa7
ryda <60327618+skVidrio@users.noreply.github.com>
2020-02-29 16:53:37 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+
  
+
 client.on('ready', () => {
+
     console.log('I am ready!');
+
 });
+
  
+
 client.on('message', message => {
+
     if (message.content === 'ping') {
+
        message.reply('pong');
+
        }
+
 });
+
  
-client.login(process.env.vHg_hPmYwBKu5O1AnpK3aoM5dndFTe-k);
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);
